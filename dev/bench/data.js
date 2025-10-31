cd59c5e23ffa34b2dd3798ad14930a778bd5098d
github-action-benchmark <github@users.noreply.github.com>
2025-10-31 14:01:31 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for a51411873b2941b29bc3baa97c5ddac3fc77f09a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761919271595,
+  "lastUpdate": 1761919291211,
   "repoUrl": "https://github.com/hulk8/cayleypy",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -129,6 +129,49 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.019863411347682533",
             "extra": "mean: 1.8564065072000004 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "khalturinmv@it.mos.ru",
+            "name": "Михаил Халтурин"
+          },
+          "committer": {
+            "email": "khalturinmv@it.mos.ru",
+            "name": "Михаил Халтурин"
+          },
+          "distinct": true,
+          "id": "a51411873b2941b29bc3baa97c5ddac3fc77f09a",
+          "message": "Use pytest marks and add benchmark jobs\n\n- Pytest runs now utilize marks for better test selection.\n- Benchmark jobs have been added to the CI pipeline.",
+          "timestamp": "2025-10-31T16:32:59+03:00",
+          "tree_id": "ce9a2ca98c2c8b0dc721f09f81b7155fc4d3bc8a",
+          "url": "https://github.com/hulk8/cayleypy/commit/a51411873b2941b29bc3baa97c5ddac3fc77f09a"
+        },
+        "date": 1761919290644,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "cayleypy/cayley_graph_test.py::test_benchmark_top_spin[26-baseline]",
+            "value": 0.13381190875249102,
+            "unit": "iter/sec",
+            "range": "stddev: 0.19135109037650544",
+            "extra": "mean: 7.473176410999997 sec\nrounds: 5"
+          },
+          {
+            "name": "cayleypy/cayley_graph_test.py::test_benchmark_top_spin[26-bit_encoded]",
+            "value": 0.5893483464182563,
+            "unit": "iter/sec",
+            "range": "stddev: 0.01517828221687334",
+            "extra": "mean: 1.6967893539999976 sec\nrounds: 5"
+          },
+          {
+            "name": "cayleypy/cayley_graph_test.py::test_benchmark_top_spin[26-bfs_numpy]",
+            "value": 0.505209358692563,
+            "unit": "iter/sec",
+            "range": "stddev: 0.01394427095469526",
+            "extra": "mean: 1.9793774260000077 sec\nrounds: 5"
           }
         ]
       }
